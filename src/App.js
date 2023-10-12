94045e2ca0b6bba7ff298a5153a78ab1b52fc072
Vibulan Jeyarasa <120412626+vibulanj@users.noreply.github.com>
2023-10-12 15:08:15 +0530

    First Commit Master

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@
 import './App.css';
 
 function App() {
+  function handleNameChange(){
+    const names = ["Earth","Grow","Give"];
+    const int =Math.floor(Math.random()*3);
+    return names[int]
+  }
   return (
     <div>
       Subscribeeee
+      <p>let's {handleNameChange()} money</p>
     </div>
   );
 }
